1b5ab5fda95d0d7ccac1e35f1ca9d1f1d34ade3e
Yuvraj Thapa <yuvrajthapa692@gmail.com>
2023-11-15 12:53:59 +0530

    initiated addition of tweet creation service

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ const connect = require('./config/database')
 
 const app = express();
 
-const TweetRepository = require('./repository/tweet-repository');
+const Tweet = require('./models/tweet');
 
 app.listen(3000, async () => {
     console.log('Server started');
     await connect();
     console.log('Mongo db connected');
-    // const tweet = await Tweet.create({
-    //     content: 'Third tweet',
-    // });
-    // const tweets = await Tweet.find({userEmail: 'yuvrajthapa692@gmail.com'});
-    const tweetRepo = new TweetRepository();
-    const tweet = await tweetRepo.create({content: 'With hooks now'});
+    const tweet = await Tweet.find({
+        content: ["First tweet"]
+    })
     console.log(tweet);
 })
