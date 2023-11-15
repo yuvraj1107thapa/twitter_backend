3e8ddf6049bc09e7aaefe68667b00ba95fd3f78c
Yuvraj Thapa <yuvrajthapa692@gmail.com>
2023-11-15 17:08:16 +0530

    Added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const connect = require('./config/database')
-
+const connect = require('./config/database');
 const app = express();
 
-const Tweet = require('./models/tweet');
-
 app.listen(3000, async () => {
-    console.log('Server started');
+    console.log('server started');
     await connect();
     console.log('Mongo db connected');
-    const tweet = await Tweet.find({
-        content: ["First tweet"]
-    })
-    console.log(tweet);
-})
+});
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,23 +1,26 @@
-const { TweetRepository } = requre('../repository/index');
+const { TweetRespository, HashtagRepository } = require('../repository/index')
 
 class TweetService {
     constructor() {
-        this.tweetRepository = new TweetRepository();
+        this.tweetRepository = new TweetRespository();
+        this.hashtagRepository = new HashtagRepository();
     }
-    
+
     async create(data) {
         const content = data.content;
-        const tags = content.match(/#[a-zA-Z0-9]+/g); // this regex extracts hashtags
-        tags == tags.map((tag) => tag.substring(1));
-        const tweet = await this.TweetRepository.create(data);
-
-        // TODO: create hashtags and add here
-        /**
-         * 1. Bulcreate in mongoose
-         * 2. Filter title of hashtag based on multiple tags
-         * 3. How to add tweet id inside all the hashtags
-         */
-        
+        const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1)); // this regex extracts hashtags
+        const tweet = await this.tweetRepository.create(data);
+        let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+        let titleOfPresenttags = alreadyPresentTags.map(tags => tags.title);
+        let newTags = tags.filter(tag => !titleOfPresenttags.includes(tag));
+        newTags = newTags.map(tag => {
+            return {title: tag, tweets: [tweet.id]}
+        });
+        await this.hashtagRepository.bulkCreate(newTags);
+        alreadyPresentTags.forEach((tag) => {
+            tag.tweets.push(tweet.id);
+            tag.save();
+        });
         return tweet;
     }
 }
